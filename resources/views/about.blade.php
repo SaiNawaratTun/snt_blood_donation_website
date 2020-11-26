4f8aaa66deac8b0d85783a5cfb7473288ab76a2e
Sai Nawarat Tun <sainawarattun123@gmail.com>
2020-11-26 14:06:37 +0630

    snt blood donation project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.show')
+
+@section('content')
+        <div class="main">
+            <div class="inner-main">
+                <div class="main-items">
+                    <img class="sticky" src="/css/img/sntBD.jpg" alt="Avatar">
+                </div>
+                <div class="main-items">
+                    <div class="dbox">
+                        <p>“SNT Blood Donation” is a blood donation center in the SNT Hospital co., Ltd. It was founded at the SNT Hospital co., Ltd in 2017 September 27. SNT Hospital co., Ltd is located in No.199 36th street Upper Block, Kyauktada Township, Yangon.</p>
+                        <p>Call Center - 01-375321, 09450594145</p>
+                        <p>Facebook Page - SNT Blood Donation</p>
+                        <div class="db">
+                            <button class="button">
+                                <a href="/feedback/create">Contact Us</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+
+@endsection
---
+++ resources/views/feedbacks/message.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>SNT Blood Donation</title>
+    <style>
+        .footer {
+            position: absolute;
+          }
+    </style>
+</head>
+<body>
+
+        <div class="container">
+            <div class="logo">
+                <h2><a href="#"><img src="/css/img/sntBD.jpg" alt=""></a></h2>
+            </div>
+
+            <div class="nav">
+                <ul class="menu">
+                    <li><a href="/"><i class="fa fa-home"></i>Home</a></li>
+                    <li><a href="/login"><i class="fa fa-user-circle" aria-hidden="true"></i>Staff</a></li>
+                    <li><a >Donate <i class="fa fa-caret-down"></i></a>
+                        <ul>
+                            <li><a href="/form/create">Blood</a></li>
+                            <li><a href="/food/create">Refreshments</a></li>
+                        </ul>
+                    </li>
+                    <li><a >Covid-19 <i class="fa fa-caret-down"></i></a>
+                        <ul>
+                            <li><a href="/item/create">Donate</a></li>
+                            <li><a href="/volunteer/create">Volunteer</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/feedback/create"><i class="fa fa-paper-plane" aria-hidden="true"></i></i>Feedback</a></li>
+                    <li><a href="/about"><i class="fa fa-info-circle" aria-hidden="true"></i>About</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="color">
+            <div class="box">
+                <h2>Sending Message Successfully!</h2>
+            </div>
+        </div>
+        
+        <!-- Foooter  -->
+      <div class="footer">
+                  <div class="copyright">
+                    <i class="fa fa-code" aria-hidden="true"> </i><strong><span> by Sai Nawarat Tun</span></strong>
+                  </div>
+          </div><!-- End Footer -->
+    
+</body>
+</html>
